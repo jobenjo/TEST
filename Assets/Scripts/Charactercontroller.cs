dd5eaed4c7a7ef9cbe350720d2a6c7bd669329b0
jobenjo <jobenjo@users.noreply.github.com>
2018-08-20 00:29:37 +0200

    Added assets

---
+++ Assets/Scripts/Charactercontroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charactercontroller : MonoBehaviour {
+    public float playerSpeed = 4f;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        GetComponent<Rigidbody2D>().velocity = targetVelocity * playerSpeed;
+    }
+}
